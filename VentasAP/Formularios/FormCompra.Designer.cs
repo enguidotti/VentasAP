630fc994d2b9dbb761cabb0f3598dd93f661c559
Enso <Gurix@DESKTOP-06R4K9S>
2021-09-29 10:51:41 -0300

    Agregar productos a la grilla, verificar existencia en la grilla

--- VentasAP/Formularios/FormCompra.Designer.cs
+++ VentasAP/Formularios/FormCompra.Designer.cs
@@ -37,15 +37,31 @@ namespace VentasAP.Formularios
             this.label3 = new System.Windows.Forms.Label();
             this.txtFactura = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
             this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.txtCantidad = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.txtCompra = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dgvDetalle = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.productoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalle)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbLocal
@@ -67,6 +83,7 @@ namespace VentasAP.Formularios
             this.btnAgregar.TabIndex = 29;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // label2
             // 
@@ -126,15 +143,36 @@ namespace VentasAP.Formularios
             this.label4.TabIndex = 34;
             this.label4.Text = "N° Factura";
             // 
-            // label5
+            // btnGuardar
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(30, 150);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(59, 20);
-            this.label5.TabIndex = 36;
-            this.label5.Text = "Cógido";
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(1114, 418);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(101, 29);
+            this.btnGuardar.TabIndex = 46;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(1085, 324);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(141, 24);
+            this.label10.TabIndex = 47;
+            this.label10.Text = "Total Compra:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(1089, 360);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(144, 31);
+            this.txtTotal.TabIndex = 48;
+            this.txtTotal.Text = "0";
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // txtCodigo
             // 
@@ -146,6 +184,16 @@ namespace VentasAP.Formularios
             this.txtCodigo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCodigo_KeyPress);
             this.txtCodigo.Leave += new System.EventHandler(this.txtCodigo_Leave);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Cógido";
+            // 
             // txtNombre
             // 
             this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -154,6 +202,15 @@ namespace VentasAP.Formularios
             this.txtNombre.ReadOnly = true;
             this.txtNombre.Size = new System.Drawing.Size(593, 26);
             this.txtNombre.TabIndex = 37;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(959, 150);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(129, 26);
+            this.txtCantidad.TabIndex = 38;
+            this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
             // 
             // label6
             // 
@@ -165,14 +222,14 @@ namespace VentasAP.Formularios
             this.label6.TabIndex = 39;
             this.label6.Text = "Cantidad";
             // 
-            // txtCantidad
+            // txtCompra
             // 
-            this.txtCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCantidad.Location = new System.Drawing.Point(959, 150);
-            this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(129, 26);
-            this.txtCantidad.TabIndex = 38;
-            this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
+            this.txtCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCompra.Location = new System.Drawing.Point(119, 203);
+            this.txtCompra.Name = "txtCompra";
+            this.txtCompra.Size = new System.Drawing.Size(129, 26);
+            this.txtCompra.TabIndex = 40;
+            this.txtCompra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCompra_KeyPress);
             // 
             // label7
             // 
@@ -184,14 +241,14 @@ namespace VentasAP.Formularios
             this.label7.TabIndex = 41;
             this.label7.Text = "P. Compra";
             // 
-            // txtCompra
+            // txtDescripcion
             // 
-            this.txtCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCompra.Location = new System.Drawing.Point(119, 203);
-            this.txtCompra.Name = "txtCompra";
-            this.txtCompra.Size = new System.Drawing.Size(129, 26);
-            this.txtCompra.TabIndex = 40;
-            this.txtCompra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCompra_KeyPress);
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(398, 200);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(690, 47);
+            this.txtDescripcion.TabIndex = 42;
             // 
             // label8
             // 
@@ -203,20 +260,98 @@ namespace VentasAP.Formularios
             this.label8.TabIndex = 43;
             this.label8.Text = "Descripción";
             // 
-            // txtDescripcion
+            // label9
             // 
-            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDescripcion.Location = new System.Drawing.Point(398, 200);
-            this.txtDescripcion.Multiline = true;
-            this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(690, 47);
-            this.txtDescripcion.TabIndex = 42;
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(353, 278);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(274, 25);
+            this.label9.TabIndex = 44;
+            this.label9.Text = "Detalle de Orden compra";
+            // 
+            // dgvDetalle
+            // 
+            this.dgvDetalle.AllowUserToAddRows = false;
+            this.dgvDetalle.AllowUserToDeleteRows = false;
+            this.dgvDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetalle.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dgvDetalle.Location = new System.Drawing.Point(34, 324);
+            this.dgvDetalle.Name = "dgvDetalle";
+            this.dgvDetalle.ReadOnly = true;
+            this.dgvDetalle.Size = new System.Drawing.Size(1032, 154);
+            this.dgvDetalle.TabIndex = 45;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Código";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cantidad";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "P. Compra";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "P. Total";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Descripción";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "IdProducto";
+            this.Column7.Name = "Column7";
+            this.Column7.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.productoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1245, 24);
+            this.menuStrip1.TabIndex = 49;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // productoToolStripMenuItem
+            // 
+            this.productoToolStripMenuItem.Name = "productoToolStripMenuItem";
+            this.productoToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.productoToolStripMenuItem.Text = "Producto";
+            this.productoToolStripMenuItem.Click += new System.EventHandler(this.productoToolStripMenuItem_Click);
             // 
             // FormCompra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1245, 450);
+            this.ClientSize = new System.Drawing.Size(1245, 490);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvDetalle);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.txtDescripcion);
             this.Controls.Add(this.label7);
@@ -234,8 +369,13 @@ namespace VentasAP.Formularios
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormCompra";
             this.Text = "FormCompra";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalle)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,14 +391,28 @@ namespace VentasAP.Formularios
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtFactura;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtTotal;
         private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtCantidad;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtCompra;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dgvDetalle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem productoToolStripMenuItem;
     }
 }
--- VentasAP/Formularios/FormCompra.cs
+++ VentasAP/Formularios/FormCompra.cs
@@ -63,7 +63,7 @@ namespace VentasAP.Formularios
 
         private void txtCodigo_Leave(object sender, EventArgs e)
         {
-            if(txtCodigo.Text.Trim() != "")
+            if (txtCodigo.Text.Trim() != "")
             {
                 verificarCodigo(txtCodigo.Text.Trim());
             }
@@ -72,7 +72,7 @@ namespace VentasAP.Formularios
         private void txtCodigo_KeyPress(object sender, KeyPressEventArgs e)
         {
             //pregunta si la tecla presionada es Enter
-            if(e.KeyChar == (char)Keys.Enter)
+            if (e.KeyChar == (char)Keys.Enter)
             {
                 if (txtCodigo.Text.Trim() != "")
                 {
@@ -94,6 +94,100 @@ namespace VentasAP.Formularios
         private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
         {
             h.soloNumeros(e);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (idProducto > 0)
+            {
+                string error = validarProducto();
+                if(error != "")
+                {
+                    MessageBox.Show(error,"Validación",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    bool existeCodigo = false;//si no existe código en la grilla se mantiene en falso
+                    //calcula el valor total(cantidad*precio_compra)
+                    int total = int.Parse(txtCantidad.Text) * int.Parse(txtCompra.Text);
+                    //verfica si la grilla trae datos
+                    if (dgvDetalle.Rows.Count > 0)
+                    {
+                        //**verifica si el producto ya esta ingresado**
+                        //se recorren las filas de la grilla
+                        foreach (DataGridViewRow f in dgvDetalle.Rows)
+                        {
+                            //verifica si el código existe en la grilla
+                            if (f.Cells[0].Value.ToString() == txtCodigo.Text)
+                            {
+                                existeCodigo = true;//existeCodigo cambia a verdadero, para no agregar un nuevo producto
+                                //ya que encuentra código, le preguntamos si desea actualziar los datos
+                                var resp = MessageBox.Show("El código ya esta ingresado,¿desea modificarlo?", "Modificar",
+                                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                                //si la respuesta es sí, se actualizan los datos
+                                if (resp == DialogResult.Yes)
+                                {
+                                    //captura el valor total 
+                                    int totalAnt = int.Parse(f.Cells[4].Value.ToString());
+                                    f.Cells[2].Value = txtCantidad.Text;
+                                    f.Cells[3].Value = txtCompra.Text;
+                                    f.Cells[4].Value = total;
+                                    f.Cells[5].Value = txtDescripcion.Text.Trim();
+                                    //al valor total se le resta el valor a modifcar y se le suma el nuevo
+                                    txtTotal.Text = (int.Parse(txtTotal.Text) + total - totalAnt).ToString();
+                                }
+                                break;//se utiliza para detener el ciclo
+                            }
+                        }
+                    }
+                    //si código es falso crear agrega un nuevo producto
+                    if (!existeCodigo)//existeCodigo == false <==> !existeCodigo
+                    {
+                        //agregar un producto a la grilla
+                        DataGridViewRow fila = new DataGridViewRow();
+                        fila.CreateCells(dgvDetalle);//se crea la fila
+                        //se agregan los elementos de la fila 
+                        fila.Cells[0].Value = txtCodigo.Text;
+                        fila.Cells[1].Value = txtNombre.Text;
+                        fila.Cells[2].Value = txtCantidad.Text;
+                        fila.Cells[3].Value = txtCompra.Text;
+                        fila.Cells[4].Value = total;
+                        fila.Cells[5].Value = txtDescripcion.Text.Trim();
+                        fila.Cells[6].Value = idProducto;
+                        //se añade la fila a grilla
+                        dgvDetalle.Rows.Add(fila);
+                        //muestra el total a pagar de nuestra orden ingresada
+                        txtTotal.Text = (int.Parse(txtTotal.Text) + total).ToString();
+                    }
+                    limpiarProducto();
+                }
+            }
+        }
+
+        private string validarProducto()
+        {
+            string error = "";
+            if (string.IsNullOrEmpty(txtCodigo.Text.Trim()))
+            {
+                error = "Debe ingresar un producto \n";
+            }
+            if (string.IsNullOrEmpty(txtCantidad.Text))
+            {
+                error += "Debe ingresar cantidad del producto \n";
+            }
+            if (string.IsNullOrEmpty(txtCompra.Text))
+            {
+                error += "Debe ingresar precio compra del producto \n";
+            }
+            return error;
+        }
+
+        private void productoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();//se oculta la ventana actual
+            FormProducto producto = new FormProducto();//se instancia la clase(formulario)
+            producto.Show();//se muestra la ventana
+
         }
     }
 
