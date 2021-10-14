5bde242b2c1f8b86e857c3084013484ebac5f0fb
Enso <Gurix@DESKTOP-06R4K9S>
2021-10-14 12:50:39 -0300

    Carrusel de colores en botones

---
+++ VentasAP/Colores.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentasAP
+{
+    public static class Colores
+    {
+        public static Color PrimaryColor { get; set; }
+        public static Color SecondaryColor { get; set; }
+        public static List<string> ColorList = new List<string>() { "#3F51B5", //azul
+                                                                    "#009688", //verdeinicio
+                                                                    "#FF5722", //naranjo
+                                                                    "#607D8B", //gris azul
+                                                                    "#FF9800", //naranjo más claro
+                                                                    "#9C27B0", //purple
+                                                                    "#2196F3", //celeste
+                                                                    "#EA676C", //rojo claro
+                                                                    "#E41A4A", //rojo
+                                                                    "#5978BB", //otro celeste
+                                                                    "#018790", //turquesa
+                                                                    "#0E3441", //azul oscuro
+                                                                    "#00B0AD", //verde más claro
+                                                                    "#721D47", //beterraga
+                                                                    "#EA4833", //otro naranjo
+                                                                    "#EF937E", //damasco
+                                                                    "#F37521", //naranjo
+                                                                    "#A12059", 
+                                                                    "#126881",
+                                                                    "#8BC240",
+                                                                    "#364D5B",
+                                                                    "#C7DC5B",
+                                                                    "#0094BC",
+                                                                    "#E4126B",
+                                                                    "#43B76E",
+                                                                    "#7BCFE9",
+                                                                    "#B71C46"};
+        public static Color ChangeColorBrightness(Color color, double correctionFactor)
+        {
+            double red = color.R;
+            double green = color.G;
+            double blue = color.B;
+
+            if (correctionFactor < 0)
+            {
+                correctionFactor = 1 + correctionFactor;
+                red *= correctionFactor;
+                green *= correctionFactor;
+                blue *= correctionFactor;
+            }
+
+            else
+            {
+                red = (255 - red) * correctionFactor + red;
+                green = (255 - green) * correctionFactor + green;
+                blue = (255 - blue) * correctionFactor + blue;
+            }
+            return Color.FromArgb(color.A, (byte)red, (byte)green, (byte)blue);
+        }
+    }
+}
--- VentasAP/FormHome.cs
+++ VentasAP/FormHome.cs
@@ -13,18 +13,70 @@ namespace VentasAP
     public partial class FormHome : Form
     {
         private Form formularioActivo;
+        private Random rnd;
+        private int indexTemp;
+        private Button botonSelect;
         public FormHome()
         {
             InitializeComponent();
+            rnd = new Random();
+        }
+
+        //seleccionar los colores de fondo 
+        private Color SeleccionColores()
+        {
+            //selecciona de manera aleatorea un índice de los colores de la lista
+            int index;
+            do
+            {
+                index = rnd.Next(Colores.ColorList.Count);
+            } while (indexTemp == index);
+            string color = Colores.ColorList[index];
+
+            return ColorTranslator.FromHtml(color);
+        }
+        //método para asignar colores al botón presionado 
+        private void BotonActivo(object btnSender)
+        {
+            if(btnSender != null)
+            {
+                BotonDesactivado();
+                //se llama el método que escoge el color
+                Color color = SeleccionColores();
+                //se asigna a la variable el botón presionado
+                botonSelect = (Button)btnSender;
+                //se le da el color de fondo al botón seleccionado
+                botonSelect.BackColor = color;
+                //las letras de botón se asignas blancas
+                botonSelect.ForeColor = Color.AntiqueWhite;
+                //cambiar el color del panel superior
+                panelTop.BackColor = color;
+
+            }
+        }
+        private void BotonDesactivado()
+        {
+            //foreach para recorrer todos los elementos del menú
+            foreach (Control btn in panelMenu.Controls)
+            {
+                //se necesita saber si el control es de tipo button
+                if(btn.GetType() == typeof(Button))
+                {
+                    btn.BackColor = Color.FromArgb(51, 51, 76);
+                    btn.ForeColor = Color.White;
+                }
+            }
         }
         //abrir formulario dentro del formulario FormHome
-        private void abrirFormulario(Form formHijo)
+        private void abrirFormulario(Form formHijo, object btnSender)
         {
             //si existe un formulario activo hijo, lo cierra
-            if(formularioActivo != null)
+            if (formularioActivo != null)
             {
                 formularioActivo.Close();
             }
+            //cada vez presiona el botón para el abrir el formulario invoca al método para cambiar colores
+            BotonActivo(btnSender);
             //se asigna el formulario hijo, al formulario que estará activo
             formularioActivo = formHijo;
             //se añaden propiedades al formulario hijo
@@ -40,22 +92,22 @@ namespace VentasAP
 
         private void btnOrden_Click(object sender, EventArgs e)
         {
-            abrirFormulario(new Formularios.FormCompra());
+            abrirFormulario(new Formularios.FormCompra(),sender);
         }
 
         private void btnProducto_Click(object sender, EventArgs e)
         {
-            abrirFormulario(new Formularios.FormProducto());
+            abrirFormulario(new Formularios.FormProducto(), sender);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            abrirFormulario(new Formularios.FormMarca());
+            abrirFormulario(new Formularios.FormMarca(), sender);
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
         {
-            if(formularioActivo != null)
+            if (formularioActivo != null)
             {
                 formularioActivo.Close();
             }
@@ -63,7 +115,7 @@ namespace VentasAP
 
         private void btnUser_Click(object sender, EventArgs e)
         {
-            abrirFormulario(new Formularios.FormUser());
+            abrirFormulario(new Formularios.FormUser(), sender);
         }
     }
 }
